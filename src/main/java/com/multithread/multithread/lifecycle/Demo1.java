aa712e1c98519119c807ac16e0b1be6906187aa5
Zhi Li <zhi.li@invoicefair.com>
2020-01-12 21:15:43 +0000

    create a new thread

--- src/main/java/com/multithread/multithread/lifecycle/Demo1.java
+++ src/main/java/com/multithread/multithread/lifecycle/Demo1.java
@@ -8,21 +8,25 @@ public class Demo1 extends Thread {
 
   @Override
   public void run() {
-    while (true)
+    while (!interrupted()) {
       System.out.println(getName() + "Thread statred...");
+    }
   }
 
   public static void main(String[] args) throws InterruptedException {
     Demo1 d1 = new Demo1("First Thread");
     Demo1 d2 = new Demo1("Second Thread");
 
-    d1.setDaemon(Boolean.TRUE);// if main thread dead, this thread dead
-    d2.setDaemon(Boolean.TRUE);
+    // if main thread dead, this thread dead
+    //    d1.setDaemon(Boolean.TRUE);
+    //    d2.setDaemon(Boolean.TRUE);
 
     d1.start();
     d2.start();
 
-    Thread.sleep(2000);
+    d1.interrupt();
+
+    // Thread.sleep(2000);
   }
 
 }
