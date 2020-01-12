bb3da13af6c7aa6aeb3f68315054808deeed881e
Zhi Li <zhi.li@invoicefair.com>
2020-01-12 20:27:19 +0000

    init

---
+++ src/main/java/com/multithread/multithread/MultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.multithread.multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadApplication.class, args);
+	}
+
+}
